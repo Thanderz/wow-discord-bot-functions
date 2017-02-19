3f4c6427d7abcf95fb4720ec79e17a2d582fef3a
Tony Tamplin <tonytamps@gmail.com>
2017-02-19 21:21:55 +1100

    change some more formatting

--- commands/help.js
+++ commands/help.js
@@ -2,18 +2,21 @@ const lambdaHandler = require('lambda-handler-as-promised');
 
 module.exports.handler = lambdaHandler(() => {
   const options = {};
-  const message = `**Available commands:**
-?help 
+  const message = `
+**?help** 
     Shows this message
-?loot 
+
+**?loot**
     Lists the rewards for doing Mythics
-?gear <character> [realm=saurfang] [region=us]
+    
+**?gear <character> [realm=saurfang] [region=us]**
     Lists the gear for the provided character name, optional realm and region
-?talents|tals <character> [realm=saurfang] [region=us] 
+
+**?talents|tals <character> [realm=saurfang] [region=us]** 
     Lists the talents for the provided character name, optional realm and region
-?prog <character> [realm=saurfang] [region=us]
-    Shows the raid progression for the provided character name, optional realm and region
-`;
+    
+**?prog <character> [realm=saurfang] [region=us]**
+    Shows the raid progression for the provided character name, optional realm and region`;
 
   return [message, options];
 });
